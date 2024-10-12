dcaef4ca014002a513f578ce6e894e32b7ac31b5
Peter Sebestyen <peti.sebes@outlook.com>
2024-10-12 13:25:04 +0200

    Created JS Files and Folders and a Structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Playlist from "./Playlist/Playlist";
+import SearchBar from "./SearchBar/SearchBar";
+import SearchResults from "./SearchResults/SearchResults";
+import Track from "./Track/Track";
+import Tracklist from "./Tracklist/Tracklist";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>Search</button>
+      <button>Save To Spotify</button>
     </div>
   );
 }
